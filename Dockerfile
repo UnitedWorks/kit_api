5ec51aaeff375d9d99a952402408ce4c9eab4a9f
Mark Hansen <x@markthemark.com>
2016-12-15 16:38:48 -0500

    Going to try running a push script for getting the image onto DockerHub instead of handling it all in the circle.yml config

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM node:6.9.2
 
-RUN mkdir -p /usr/src/app
-WORKDIR /usr/src/app
+RUN mkdir -p /usr/src/mayorapi
+WORKDIR /usr/src/mayorapi
 
-COPY . /usr/src/app
+COPY . /usr/src/mayorapi
 RUN npm install
 
 EXPOSE 5000
---
+++ manage.js
@@ -0,0 +1,52 @@
+// Pull in Environment Variables
+require('dotenv').config();
+const ps = require('child_process');
+
+// Break up Args
+const env = process.env;
+const args = process.argv.slice(2);
+const command = args[0];
+
+const simpleLog = (err, out, msg) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(out);
+    console.log(msg);
+  }
+};
+
+// Commands
+if (command === 'push') {
+  const repoRoot = process.env.DOCKER_REPO_ROOT;
+  const service = args[1];
+  ps.exec('git rev-parse HEAD', (err, stdout) => {
+    // Get hash of last commit
+    const gitHash = stdout;
+    // Login
+    ps.exec(`docker login -e ${env.DOCKER_HUB_EMAIL} -u ${env.DOCKER_HUB_USER} -p ${env.DOCKER_HUB_PWD}`, (logInErr, logInOut) => {
+      simpleLog(logInErr, logInOut, `Logged In. Pushing ${repoRoot}_${service} with tags 'latest' and '${gitHash}' to docker.io/${env.DOCKER_HUB_ORG}`);
+      if (logInErr) return;
+      // Tag Latest
+      ps.exec(`docker tag ${repoRoot}_${service} ${env.DOCKER_HUB_USER}/${repoRoot}_${service}:latest`, (latestTagErr, latestTagOut) => {
+        simpleLog(latestTagErr, latestTagOut, 'Latest Tagged');
+        // Push
+        ps.exec(`docker push ${env.DOCKER_HUB_USER}/${repoRoot}_${service}:latest`, (latestPushErr, latestPushOut) => {
+          simpleLog(latestPushErr, latestPushOut, 'Latest Pushed');
+        });
+      });
+      // Tag Hashed
+      ps.exec(`docker tag ${repoRoot}_${service} ${env.DOCKER_HUB_USER}/${repoRoot}_${service}:${gitHash}`, (hashedTagErr, hashedTagOut) => {
+        simpleLog(hashedTagErr, hashedTagOut, 'Hashed Tagged');
+        // Push
+        ps.exec(`docker push ${env.DOCKER_HUB_USER}/${repoRoot}_${service}:${gitHash}`, (hashedPushErr, hashedPushOut) => {
+          simpleLog(hashedPushErr, hashedPushOut, 'Hashed Pushed');
+        });
+      });
+    });
+  });
+}
+
+if (command === 'deploy') {
+  console.log('Deploying');
+}
