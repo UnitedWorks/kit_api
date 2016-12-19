2e9fb6dceed1f4c56c8756c51a5ef707ad2abf9e
Mark Hansen <x@markthemark.com>
2016-12-19 00:40:24 -0500

    Take two at blowing everything up

---
+++ deploy/circle_ci_deploy.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# more bash-friendly output for jq
+JQ="jq --raw-output --exit-status"
+
+configure_aws_cli(){
+	aws --version
+	aws configure set default.region us-east-1
+	aws configure set default.output json
+}
+
+deploy_cluster() {
+
+    make_task_def
+    register_definition
+    if [[ $(aws ecs update-service --cluster kit_production --service api --task-definition $revision | \
+                   $JQ '.service.taskDefinition') != $revision ]]; then
+        echo "Error updating service."
+        return 1
+    fi
+
+    echo "Service update took too long."
+    return 1
+}
+
+make_task_def(){
+	task_template=$(cat api-task-definition.json)
+	task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CIRCLE_SHA1)
+}
+
+push_ecr_image(){
+	eval $(aws ecr get-login --region us-east-1)
+	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/kit_api/api:$CIRCLE_SHA1
+}
+
+register_definition() {
+
+    if revision=$(aws ecs register-task-definition --cli-input-json "$task_def"); then
+        echo "Revision: $revision"
+    else
+        echo "Failed to register task definition"
+        return 1
+    fi
+
+}
+
+configure_aws_cli
+push_ecr_image
+deploy_cluster
