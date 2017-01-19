45dc863424d3873bb8a2550f8aef71998a5df401
Mark Hansen <x@markthemark.com>
2017-01-19 18:28:35 -0500

    fixed routes. cant branch off of route

--- api/cases/routes.js
+++ api/cases/routes.js
@@ -4,11 +4,16 @@ import * as process from './helpers';
 
 const router = new Router();
 
-router.route('/webhook/email')
-  .post((req, res) => {
-    logger.info('Case Email Webhook Pinged');
-    process.webhookHitWithEmail(req);
-    res.status(200).send();
-  });
+router.post('/webhook/email', (req, res) => {
+  logger.info('Case Email Webhook Pinged');
+  process.webhookHitWithEmail(req);
+  res.status(200).send();
+});
+
+router.post('/webhook/event', (req, res) => {
+  logger.info('Case Event Webhook Pinged');
+  logger.info(req.body);
+  res.status(200).send();
+});
 
 module.exports = router;
