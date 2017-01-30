79559246e766c1e923ee58daf67f8f9cc0cac727
Mark Hansen <x@markthemark.com>
2017-01-30 13:28:52 -0500

    Updated knowledge base schema to split knowledge_questions out of knowledge_answers. Also created a junction table that I think will help with searches

--- migrations/versions/20161230224034_knowledge.js
+++ migrations/versions/20161230224034_knowledge.js
@@ -14,7 +14,7 @@ exports.up = function(knex, Promise) {
       table.increments('id').primary();
       table.string('name');
       table.text('description');
-      table.integer('category_id')
+      table.integer('knowledge_category_id')
         .unsigned().references('id').inTable('knowledge_categorys');
       table.integer('type_id')
         .unsigned().references('id').inTable('knowledge_facility_types');
@@ -30,13 +30,13 @@ exports.up = function(knex, Promise) {
       table.increments('id').primary();
       table.string('name');
       table.text('description');
-      table.integer('category_id')
+      table.integer('knowledge_category_id')
         .unsigned().references('id').inTable('knowledge_categorys');
       table.integer('schedule_id')
         .unsigned().references('id').inTable('schedules');
       table.integer('location_id')
         .unsigned().references('id').inTable('locations');
-      table.integer('facility_id')
+      table.integer('knowledge_facility_id')
         .unsigned().references('id').inTable('knowledge_facilitys');
       table.integer('organization_id')
         .unsigned().references('id').inTable('organizations');
@@ -46,13 +46,13 @@ exports.up = function(knex, Promise) {
       table.increments('id').primary();
       table.string('name');
       table.text('description');
-      table.integer('category_id')
+      table.integer('knowledge_category_id')
         .unsigned().references('id').inTable('knowledge_categorys');
       table.integer('schedule_id')
         .unsigned().references('id').inTable('schedules');
       table.integer('location_id')
         .unsigned().references('id').inTable('locations');
-      table.integer('facility_id')
+      table.integer('knowledge_facility_id')
         .unsigned().references('id').inTable('knowledge_facilitys');
       table.integer('service_id')
         .unsigned().references('id').inTable('knowledge_services');
@@ -60,19 +60,34 @@ exports.up = function(knex, Promise) {
         .unsigned().references('id').inTable('organizations');
       table.dateTime('created_at').defaultTo(knex.raw('now()'));
     })
-    .createTable('knowledge_answers', (table) => {
+    .createTable('knowledge_questions', (table) => {
       table.increments('id').primary();
       table.string('label');
-      table.text('question');
-      table.text('answer');
-      table.integer('category_id')
+      table.integer('knowledge_category_id')
         .unsigned().references('id').inTable('knowledge_categorys');
-      table.string('url');
+      table.text('question');
+      table.dateTime('created_at').defaultTo(knex.raw('now()'));
+    })
+    .createTable('knowledge_answers', (table) => {
+      table.increments('id').primary();
+      table.integer('knowledge_question_id')
+        .unsigned().references('id').inTable('organizations');
       table.integer('organization_id')
         .unsigned().references('id').inTable('organizations');
+      table.text('answer');
+      table.string('url');
       table.dateTime('created_at').defaultTo(knex.raw('now()'));
     })
     // Junction Tables
+    .createTable('knowledge_questions_organizations_knowledge_answers', (table) => {
+      table.integer('knowledge_question_id')
+        .unsigned().references('id').inTable('knowledge_questions');
+      table.integer('organization_id')
+        .unsigned().references('id').inTable('organizations');
+      table.integer('knowledge_answer_id')
+        .unsigned().references('id').inTable('knowledge_answers');
+      table.dateTime('created_at').defaultTo(knex.raw('now()'));
+    })
     .createTable('knowledge_answers_knowledge_events', (table) => {
       table.increments('id').primary();
       table.integer('knowledge_answer_id').notNullable()
@@ -98,9 +113,11 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return knex.schema
+    .dropTable('knowledge_questions_organizations_knowledge_answers')
     .dropTable('knowledge_answers_knowledge_events')
     .dropTable('knowledge_answers_knowledge_services')
     .dropTable('knowledge_answers_knowledge_facilitys')
+    .dropTable('knowledge_questions')
     .dropTable('knowledge_answers')
     .dropTable('knowledge_events')
     .dropTable('knowledge_services')
