d2ebebfad88c165af2b41a2682673b1dcf9fc3c8
Mark Hansen <x@markthemark.com>
2017-02-25 15:48:52 -0500

    Updated complaint states. Fixed action obj definition

--- api/narratives/states/complaint-states.js
+++ api/narratives/states/complaint-states.js
@@ -4,89 +4,101 @@ import { createCase } from '../../cases/helpers';
 import { CaseCategory } from '../../cases/models';
 
 export default {
-  complaints_init(aux = {}) {
-    const quickReplies = PRIMARY_CASE_CATEGORIES.map((label) => {
-      return {
-        content_type: 'text',
-        title: label,
-        payload: label,
-      };
-    });
-
-    return this.messagingClient.send(aux.message || 'What type of problem do you have?', quickReplies).then(() => {
-      return 'waiting_for_complaint';
-    });
-  },
-
-  complaintCategory() {
-    // Check for text answer or passed quick_reply payload
-    const category = this.get('input').payload.text || this.get('input').payload.payload;
-    CaseCategory.where({ parent_category_id: null }).fetchAll().then((data) => {
-      let foundModel;
-      let generalModel;
-      data.models.forEach((model) => {
-        if (model.get('label') === 'General') generalModel = model.toJSON();
-        if (model.get('label') === category) foundModel = model.toJSON();
+  waiting_for_complaint: {
+    enter(aux = {}) {
+      const quickReplies = PRIMARY_CASE_CATEGORIES.map((label) => {
+        return {
+          content_type: 'text',
+          title: label,
+          payload: label,
+        };
       });
-      const complaint = { category: foundModel || generalModel };
-      this.set('complaint', complaint);
-      this.messagingClient.send('Can you describe the problem for me?');
-      this.exit('complaintText');
-    });
-  },
-
-  complaintText() {
-    const headline = this.get('input').payload.text;
-    if (headline) {
-      this.set('complaint', Object.assign({}, this.get('complaint'), { headline }));
-      this.messagingClient.send('Can you provide a picture? If not, simply say you don\'t have one');
-      this.exit('complaintPicture');
-    }
-  },
-
-  complaintPicture() {
-    const payload = this.get('input').payload;
-    if (payload.attachments) {
-      this.messagingClient.send('Thank you!');
-      const updatedComplaint = Object.assign({}, this.get('complaint'), {
-        attachments: payload.attachments,
+      return this.messagingClient.send(aux.message || 'What type of problem do you have?', quickReplies);
+    },
+    message() {
+      // Check for text answer or passed quick_reply payload
+      const category = this.get('input').payload.text || this.get('input').payload.payload;
+      return CaseCategory.where({ parent_category_id: null }).fetchAll().then((data) => {
+        let foundModel;
+        let generalModel;
+        data.models.forEach((model) => {
+          if (model.get('label') === 'General') generalModel = model.toJSON();
+          if (model.get('label') === category) foundModel = model.toJSON();
+        });
+        const complaint = { category: foundModel || generalModel };
+        this.set('complaint', complaint);
+        return 'waiting_for_complaint_description';
       });
-      this.set('complaint', updatedComplaint);
-    }
-    this.messagingClient.send('Can you send your location? That can simply be an address or a pin on the map.');
-    this.exit('complaintLocation');
+    },
   },
 
-  complaintLocation() {
-    const payload = this.get('input').payload;
-    if (payload.attachments) {
-      const updatedComplaint = Object.assign({}, this.get('complaint'), {
-        location: {
-          latitude: payload.attachments[0].payload.coordinates.lat,
-          longitude: payload.attachments[0].payload.coordinates.long,
-        },
-      });
-      this.set('complaint', updatedComplaint);
-      this.fire('complaintSubmit');
-    } else if (payload.text) {
-      geocoder.geocode(payload.text).then((geoData) => {
+  waiting_for_complaint_description: {
+    enter() {
+      return this.messagingClient.send('Can you describe the problem for me?');
+    },
+    message() {
+      const headline = this.get('input').payload.text;
+      if (headline) {
+        this.set('complaint', Object.assign({}, this.get('complaint'), { headline }));
+      }
+      return 'wait_for_complaint_picture';
+    },
+  },
+
+  wait_for_complaint_picture: {
+    enter() {
+      return this.messagingClient.send('Can you provide a picture? If not, simply say you don\'t have one');
+    },
+    message() {
+      const payload = this.get('input').payload;
+      if (payload.attachments) {
+        this.messagingClient.send('Thank you!');
         const updatedComplaint = Object.assign({}, this.get('complaint'), {
-          location: geoData[Object.keys(geoData)[0]],
+          attachments: payload.attachments,
         });
         this.set('complaint', updatedComplaint);
-        this.fire('complaintSubmit');
-      });
-    } else {
-      this.fire('complaintSubmit');
-    }
+      }
+      return 'wait_for_complaint_location';
+    },
   },
 
-  complaintSubmit() {
-    const complaint = this.get('complaint');
-    createCase(complaint.headline, complaint.data, complaint.category, this.snapshot.constituent, this.get('organization'), complaint.location, complaint.attachments).then(() => {
-      this.messagingClient.send('I just sent your message along. I\'ll try to let you know when it\'s been addressed.');
-      this.exit('start');
-    });
+  wait_for_complaint_location: {
+    enter() {
+      this.messagingClient.send('Can you send your location? That can simply be an address or a pin on the map.');
+    },
+    message() {
+      const payload = this.get('input').payload;
+      if (payload.attachments) {
+        const updatedComplaint = Object.assign({}, this.get('complaint'), {
+          location: {
+            latitude: payload.attachments[0].payload.coordinates.lat,
+            longitude: payload.attachments[0].payload.coordinates.long,
+          },
+        });
+        this.set('complaint', updatedComplaint);
+        return 'complaint_submit';
+      } else if (payload.text) {
+        return geocoder.geocode(payload.text).then((geoData) => {
+          const updatedComplaint = Object.assign({}, this.get('complaint'), {
+            location: geoData[Object.keys(geoData)[0]],
+          });
+          this.set('complaint', updatedComplaint);
+          return 'complaint_submit';
+        });
+      }
+      return 'complaint_submit';
+    },
+  },
+
+  complaint_submit: {
+    enter() {
+      const complaint = this.get('complaint');
+      return createCase(complaint.headline, complaint.data, complaint.category, this.snapshot.constituent, this.get('organization'), complaint.location, complaint.attachments)
+        .then(() => {
+          this.messagingClient.send('I just sent your message along. I\'ll try to let you know when it\'s been addressed.');
+          return 'smallTalk.start';
+        });
+    },
   },
 
 };
