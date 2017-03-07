48ce61acbaf6499deafe6e0a5d70fbc0ef6fa9c7
Mark Hansen <x@markthemark.com>
2017-03-07 18:17:46 -0500

    Specify response type for FB/SMS files. No more libs

--- api/services/aws.js
+++ api/services/aws.js
@@ -1,5 +1,5 @@
 import AWS from 'aws-sdk';
-import Jimp from 'jimp';
+import axios from 'axios';
 import { PRODUCTION } from '../constants/environments';
 
 export default class AWSHelper {
@@ -13,19 +13,20 @@ export default class AWSHelper {
   copyExternalUrlToS3(url, directory) {
     const self = this;
     return new Promise((resolve, reject) => {
-      Jimp.read(url).then((image) => {
-        image.quality(60).getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-          const placement = directory ? self.bucket.concat(`/${directory}`) : self.bucket.concat('/uploads');
-          self.s3.upload({
-            ACL: 'public-read',
-            Bucket: placement,
-            Key: /[^/]+$/i.exec(url)[0],
-            ContentType: image._originalMime,
-            Body: buffer,
-          }, (s3Error, data) => {
-            if (s3Error) reject(s3Error);
-            resolve(data.Location);
-          });
+      axios.get(url, {
+        responseType: 'arraybuffer',
+      }).then((response) => {
+        const placement = directory ? self.bucket.concat(`/${directory}`) : self.bucket.concat('/uploads');
+        self.s3.upload({
+          ACL: 'public-read',
+          Bucket: placement,
+          Key: /[^/]+$/i.exec(url)[0],
+          ContentEncoding: 'utf8',
+          ContentType: response.headers['content-type'],
+          Body: new Buffer(response.data, 'binary'),
+        }, (s3Error, data) => {
+          if (s3Error) reject(s3Error);
+          resolve(data.Location);
         });
       });
     });
