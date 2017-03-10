77cb1c2fbf5152409f07999a7c62cc7a493e6568
Mark Hansen <x@markthemark.com>
2017-03-10 10:59:34 -0500

    Expand knowledge base models based on expected exepected Open311 responses

---
+++ migrations/versions/20170310101810_expand-knowledge.js
@@ -0,0 +1,56 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .alterTable('knowledge_answers', (table) => {
+      table.dropUnique(['organization_id', 'question_id']);
+      table.dateTime('expiration');
+    })
+    .alterTable('knowledge_facilitys', (table) => {
+      table.text('breief_description');
+      table.text('eligibility_information');
+      table.string('url');
+      table.string('phone_number');
+    })
+    .alterTable('knowledge_services', (table) => {
+      table.text('breief_description');
+      table.dateTime('expiration');
+      table.string('url');
+      table.string('phone_number');
+    })
+    .alterTable('knowledge_events', (table) => {
+      table.text('breief_description');
+      table.string('url');
+      table.string('phone_number');
+    })
+    .alterTable('medias', (table) => {
+      table.string('original_url');
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .alterTable('knowledge_answers', (table) => {
+      table.unique(['organization_id', 'question_id']);
+      table.dropColumn('expiration');
+    })
+    .alterTable('knowledge_facilitys', (table) => {
+      table.dropColumn('breief_description');
+      table.dropColumn('eligibility_information');
+      table.dropColumn('url');
+      table.dropColumn('phone_number');
+    })
+    .alterTable('knowledge_services', (table) => {
+      table.dropColumn('breief_description');
+      table.dropColumn('expiration');
+      table.dropColumn('url');
+      table.dropColumn('phone_number');
+    })
+    .alterTable('knowledge_events', (table) => {
+      table.dropColumn('breief_description');
+      table.dropColumn('url');
+      table.dropColumn('phone_number');
+    })
+    .alterTable('medias', (table) => {
+      table.dropColumn('original_url');
+    });
+};
