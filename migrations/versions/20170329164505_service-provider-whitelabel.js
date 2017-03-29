4add4668e0cad3cfec1da55f9e1dcd09d80fc40b
Mark Hansen <x@markthemark.com>
2017-03-29 12:50:09 -0400

    Alter type/category on organization model

---
+++ migrations/versions/20170329164505_service-provider-whitelabel.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .alterTable('organizations', (table) => {
+      table.dropColumn('abbreviation');
+      table.dropColumn('category');
+      table.dropColumn('type');
+    })
+    .alterTable('organizations', (table) => {
+      table.enum('type', ['government', 'provider']).defaultTo('government');
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .alterTable('organizations', (table) => {
+      table.string('abbreviation');
+      table.enum('category', ['public', 'ngo', 'private']);
+      table.enum('type', ['admin', 'division']);
+    });
+};
