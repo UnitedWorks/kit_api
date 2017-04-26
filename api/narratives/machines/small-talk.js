0d6b18cea39d164b730db2011b4b5acbcc32a7f1
Mark Hansen <x@markthemark.com>
2017-04-26 16:35:53 -0700

    Transform postback payloads using start case to have spaces

--- api/narratives/machines/small-talk.js
+++ api/narratives/machines/small-talk.js
@@ -239,8 +239,10 @@ export default {
   // TODO(nicksahler): Move to init
   start: {
     message() {
-      const input = this.snapshot.input.payload;
-      return nlp.message(input.text, {}).then((nlpData) => {
+      if (!this.snapshot.input.payload.text && this.snapshot.input.payload.payload) {
+        this.snapshot.input.payload.text = this.snapshot.input.payload.payload.replace(/([A-Z])/g, ' $1').trim();
+      }
+      return nlp.message(this.snapshot.input.payload.text, {}).then((nlpData) => {
         this.snapshot.nlp = nlpData;
 
         logger.info(nlpData);
