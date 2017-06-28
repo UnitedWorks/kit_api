01aac5c64d715028ac97696dca53858fbd09dcd0
Mark Hansen <x@markthemark.com>
2017-06-27 22:42:07 -0400

    Ping slack when an answer was not helpful

--- api/narratives/machines/eval.js
+++ api/narratives/machines/eval.js
@@ -1,4 +1,5 @@
 import Mixpanel from '../../services/event-tracking';
+import SlackService from '../../services/slack';
 
 export default {
   answer_helpful() {
@@ -6,18 +7,22 @@ export default {
       distinct_id: this.snapshot.constituent.id,
       constituent_id: this.snapshot.constituent.id,
       organization_id: this.get('organization').id,
-      helpful: 5,
       interface: this.messagingClient.provider,
+      helpful: 5,
     });
     return 'personality.handle_thank_you';
   },
   answer_not_helpful() {
+    new SlackService({
+      username: 'Answer Not Helpful',
+      icon: 'disappointed',
+    }).send(`>*Constituent ID*: ${this.snapshot.constituent.id}`);
     Mixpanel.track('answer_feedback', {
       distinct_id: this.snapshot.constituent.id,
       constituent_id: this.snapshot.constituent.id,
       organization_id: this.get('organization').id,
-      helpful: 0,
       interface: this.messagingClient.provider,
+      helpful: 0,
     });
     return this.messagingClient.send('Sorry about that. My little bot brain is doing its best :/ Hope I can do better next time!')
       .then(() => this.getBaseState());
