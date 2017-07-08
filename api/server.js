4cd19addd099eca65807b6e11e209eeff4d60743
Mark Hansen <x@markthemark.com>
2017-07-07 21:44:25 -0400

    Allow for 3000 port for chat

--- api/server.js
+++ api/server.js
@@ -22,7 +22,9 @@ app.use(bodyParser.json({
 // Handle Cross Origin Options
 const whitelist = [
   'http://localhost:8000',
+  'http://localhost:3000',
   'http://127.0.0.1:8000',
+  'http://127.0.0.1:3000',
   'https://dashboard.kit.community',
 ];
 const corsOptions = {
