951f2a60c069f31a0f59cfbbbd55aef11b876c41
Mark Hansen <x@markthemark.com>
2017-07-10 15:41:38 -0400

    Hidden rep field causing problem with password comparison at login. Need to later do this and make sure I'm not breaking other things

--- api/accounts/models.js
+++ api/accounts/models.js
@@ -24,7 +24,7 @@ export const Representative = bookshelf.Model.extend({
       }
     });
   },
-  hidden: ['_pivot_knowledge_category_id', '_pivot_representative_id', 'password', 'email_confirmed', 'created_at'],
+  hidden: ['_pivot_knowledge_category_id', '_pivot_representative_id', 'email_confirmed', 'created_at'],
   organization() {
     return this.belongsTo(Organization, 'organization_id');
   },
