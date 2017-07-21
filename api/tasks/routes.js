66f51ebacb40524ac09363c99e9859e1b5860a9d
Mark Hansen <x@markthemark.com>
2017-07-20 21:09:00 -0400

    Task update route

--- api/tasks/routes.js
+++ api/tasks/routes.js
@@ -20,6 +20,18 @@ router.route('/')
     } catch (e) {
       next(e);
     }
+  })
+  .put((req, res, next) => {
+    try {
+      Task.where({ id: req.body.id }).save(req.body, { method: 'update', patch: true })
+        .then((updated) => {
+          updated.refresh().then((refreshedTask) => {
+            res.status(200).send({ task: refreshedTask.toJSON() });
+          });
+        }).catch(err => next(err));
+    } catch (e) {
+      next(e);
+    }
   });
 
 module.exports = router;
