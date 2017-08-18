3e9caf6fb3ea5dd27c1b7afa35c0b258cc51047b
Mark Hansen <x@markthemark.com>
2017-08-18 11:35:45 -0400

    Shit thankfully didnt do anything. Just clearing for now

--- migrations/versions/20170724001407_temporary-answers.js
+++ migrations/versions/20170724001407_temporary-answers.js
@@ -1,14 +1,9 @@
 
 
 exports.up = function(knex, Promise) {
-    knex.schema.alterTable('knowledge_facilitys', (table) => {
- 	   table.integer('event_rule_id').references('event_rule.id');
- 	})
- 	.alterTable('event_rules', (table) => {
 
- 	});
 };
 
 exports.down = function(knex, Promise) {
-  
+
 };
