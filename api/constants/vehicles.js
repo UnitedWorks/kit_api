8d28b2fd9a0532f8d01bb3bf24c6f5ffc30b0928
Mark Hansen <x@markthemark.com>
2017-11-21 21:49:06 -0500

    Vehicle function constants

---
+++ api/constants/vehicles.js
@@ -0,0 +1,14 @@
+export const GAS = 'gas';
+export const DIESEL = 'diesel';
+export const ELECTRIC = 'electric';
+
+export const SNOW_REMOVAL = 'snow_removal';
+export const FIRE_EMERGENCY = 'fire_emergency';
+export const MEDICAL_EMERGENCY = 'medical_emergency';
+export const SAFETY_EMERGENCY = 'safety_emergency';
+export const STREET_SWEEPING = 'street_sweeping';
+export const TRASH_COLLECTION = 'trash_collection';
+export const RECYCLING_COLLECTION = 'recycling_collection';
+
+export const VEHICLE_FUNCTIONS = [SNOW_REMOVAL, FIRE_EMERGENCY, MEDICAL_EMERGENCY,
+  SAFETY_EMERGENCY, STREET_SWEEPING, TRASH_COLLECTION, RECYCLING_COLLECTION];
