f26842f9b4c8795ac71e506e4ab57602a9f67d39
Mark Hansen <x@markthemark.com>
2017-11-28 10:07:47 -0500

    Ask for feedback if someone gives a down thumb

--- api/narratives/machines/eval.js
+++ api/narratives/machines/eval.js
@@ -15,17 +15,26 @@ export default {
     }
     return 'personality.handle_thank_you';
   },
-  answer_not_helpful() {
-    new SlackService({
-      username: 'Not Helpful',
-      icon: 'disappointed',
-    }).send(`>*Con. ${this.snapshot.constituent.id}*`);
-    try {
-      EventTracker('answer_feedback', { session: this }, { helpful: 0 });
-    } catch (e) {
-      logger.error(e);
-    }
-    return this.messagingClient.send('Sorry about that :/ Hope I can do better next time!')
-      .then(() => this.getBaseState());
+  answer_not_helpful: {
+    enter() {
+      new SlackService({
+        username: 'Not Helpful',
+        icon: 'disappointed',
+      }).send(`>*Con. ${this.snapshot.constituent.id}*`);
+      try {
+        EventTracker('answer_feedback', { session: this }, { helpful: 0 });
+      } catch (e) {
+        logger.error(e);
+      }
+      this.messagingClient.send('Sorry about that. Why wasn\'t this answer helpful?');
+    },
+    message() {
+      new SlackService({
+        username: 'Not Helpful',
+        icon: 'disappointed',
+      }).send(`>*Con. ${this.snapshot.constituent.id}*  Feedback - ${this.snapshot.input.payload.text}`);
+      return this.messagingClient.send('Thank you for the feedback. Will make not of that and try improving.')
+        .then(() => this.getBaseState());
+    },
   },
 };
