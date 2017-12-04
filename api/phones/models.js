a180f6a931ef4b6569f8770aa8161ef35446c1ff
Mark Hansen <x@markthemark.com>
2017-12-04 01:55:21 -0500

    Hide pivots

--- api/phones/models.js
+++ api/phones/models.js
@@ -3,6 +3,7 @@ import { Organization } from '../accounts/models';
 
 export const Phone = bookshelf.Model.extend({
   tableName: 'phones',
+  hidden: ['_pivot_phone_id', '_pivot_service_id'],
   organization() {
     return this.belongsTo(Organization, 'organization_id');
   },
