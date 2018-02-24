7f9b27d2ec533f9c4da1dc17663a4a79136c8953
Mark Hansen <x@markthemark.com>
2018-02-24 17:55:04 -0500

    More intents

--- nlp/data/index.js
+++ nlp/data/index.js
@@ -1,13 +1,13 @@
 module.exports = {
   rasa_nlu_data: {
     common_examples: [
-      // ...require('./v2/business_finance'),
+      ...require('./v2/business_finance'),
       // ...require('./v2/constituent_attributes'),
       // ...require('./v2/education_employment'),
       // ...require('./v2/environment_sanitation'),
-      ...require('./v2/escalate'),
+      // ...require('./v2/escalate'),
       // ...require('./v2/general'),
-      // ...require('./v2/government_civil_services'),
+      ...require('./v2/government_civil_services'),
       // ...require('./v2/health_medicine'),
       // ...require('./v2/interaction'),
       // ...require('./v2/locale'),
@@ -17,7 +17,7 @@ module.exports = {
       // ...require('./v2/public_safety_law'),
       // ...require('./v2/search'),
       // ...require('./v2/settings'),
-      ...require('./v2/social_services'),
+      // ...require('./v2/social_services'),
       // ...require('./v2/speech'),
       // ...require('./v2/transportation_streets_sidewalks'),
       // ...require('./v2/voting_elections_participation'),
